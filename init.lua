bf7a57c1031c4b6d6ffe61edeac2b84895b6d650
João Pedro <contas.joaopedro.favoretti@gmail.com>
2025-07-17 20:38:25 -0300

    Adding nerd font support and maximize command

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -168,6 +168,8 @@ vim.o.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+
+vim.keymap.set('n', '<leader>m', '<cmd>tab sp<CR>', { desc = '[M]aximize window (tab split)' })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
@@ -262,6 +264,15 @@ require('lazy').setup({
   'github/copilot.vim',
   'mg979/vim-visual-multi',
   'nvim-tree/nvim-web-devicons',
+  -- {
+  --   'm4xshen/autoclose.nvim',
+  --   opts = {
+  --     close = true,
+  --     options = {
+  --       disabled_filetypes = { 'text', 'markdown' },
+  --     },
+  --   },
+  -- },
 
   {
     'nvim-tree/nvim-tree.lua',
@@ -314,26 +325,6 @@ require('lazy').setup({
       null_ls.setup {
         sources = {
           null_ls.builtins.formatting.prettierd,
-        },
-      }
-    end,
-  },
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    opts = function()
-      local null_ls = require 'null-ls'
-      return {
-        sources = {
-          -- null_ls.builtins.code_actions.eslint,
-          null_ls.builtins.code_actions.eslint_d,
-          -- null_ls.builtins.diagnostics.eslint,
-          -- null_ls.builtins.diagnostics.eslint_d,
-          -- null_ls.builtins.formatting.eslint,
-          -- null_ls.builtins.formatting.eslint_d,
-          null_ls.builtins.formatting.prettier_eslint,
         },
       }
     end,
@@ -1038,27 +1029,27 @@ require('lazy').setup({
     },
   },
 
---   { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    -- 'folke/tokyonight.nvim',
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- config = function()
-    --   ---@diagnostic disable-next-line: missing-fields
-    --   require('tokyonight').setup {
-    --     styles = {
-    --       comments = { italic = false }, -- Disable italics in comments
-    --     },
-    --   }
+  --   { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  -- 'folke/tokyonight.nvim',
+  -- priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- config = function()
+  --   ---@diagnostic disable-next-line: missing-fields
+  --   require('tokyonight').setup {
+  --     styles = {
+  --       comments = { italic = false }, -- Disable italics in comments
+  --     },
+  --   }
 
-    --   -- Load the colorscheme here.
-    --   -- Like many other themes, this one has different styles, and you could load
-    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    --   vim.cmd.colorscheme 'tokyonight-night'
-    -- end,
-    {
+  --   -- Load the colorscheme here.
+  --   -- Like many other themes, this one has different styles, and you could load
+  --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --   vim.cmd.colorscheme 'tokyonight-night'
+  -- end,
+  {
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = true,
